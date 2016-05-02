4b562a11f3f491c139b8948f7bc4d524a958a2cd
algo13 <algo13.net@gmail.com>
2016-05-02 21:29:48 +0900

    Changed 2.1.0

--- src/PhpNag/Lexer.php
+++ src/PhpNag/Lexer.php
@@ -16,39 +16,45 @@ class Lexer extends \PhpParser\Lexer
                 // EOF token with ID 0
                 $token = "\0";
             }
-            if (is_string($token)) {
-                // bug in token_get_all
-                if ('b"' === $token) {
-                    $value = 'b"';
+            $startAttributes['startLine'] = $this->line;
+            //$startAttributes['startTokenPos'] = $this->pos;
+            //$startAttributes['startFilePos'] = $this->filePos;
+            if (\is_string($token)) {
+                $value = $token;
+                if (isset($token[1])) {
+                    // bug in token_get_all
                     $this->filePos += 2;
                     $id = ord('"');
                 } else {
-                    $value = $token;
                     $this->filePos += 1;
                     $id = ord($token);
                 }
-                $startAttributes['startLine'] = $this->line;
-                //$endAttributes['endLine'] = $this->line;
-                //$endAttributes['endTokenPos'] = $this->pos;
-                //$endAttributes['endFilePos'] = $this->filePos - 1;
-                return $id;
+            } elseif (!isset($this->dropTokens[$token[0]])) {
+                $value = $token[1];
+                $id = $this->tokenMap[$token[0]];
+
+                $this->line += substr_count($value, "\n");
+                $this->filePos += \strlen($value);
             } else {
-                $this->line += substr_count($token[1], "\n");
-                $this->filePos += strlen($token[1]);
+                //if (isset($this->usedAttributes['comments'])) {
                 if (T_COMMENT === $token[0]) {
-                    $startAttributes['comments'][] = new Comment($token[1], $token[2]);
+                    $startAttributes['comments'][] = new Comment($token[1], $this->line, $this->filePos);
                 } elseif (T_DOC_COMMENT === $token[0]) {
-                    $startAttributes['comments'][] = new Comment\Doc($token[1], $token[2]);
-                } elseif (!isset($this->dropTokens[$token[0]])) {
-                    $value = $token[1];
-                    $startAttributes['startLine'] = $token[2];
-                    //$endAttributes['endLine'] = $this->line;
-                    //$endAttributes['endTokenPos'] = $this->pos;
-                    //$endAttributes['endFilePos'] = $this->filePos - 1;
-                    return $this->tokenMap[$token[0]];
+                    $startAttributes['comments'][] = new Comment\Doc($token[1], $this->line, $this->filePos);
+                } else {
+                    // noop
                 }
+                //}
+                $this->line += substr_count($token[1], "\n");
+                $this->filePos += \strlen($token[1]);
+                continue;
             }
+            //$endAttributes['endLine'] = $this->line;
+            //$endAttributes['endTokenPos'] = $this->pos;
+            //$endAttributes['endFilePos'] = $this->filePos - 1;
+            return $id;
         }
+
         throw new \RuntimeException('Reached end of lexer loop');
     }
 }
